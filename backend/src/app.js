fe02b912f2796b8facad3b21eaa173e6111df80f
Kumar manak <manakkash20@gmail.com>
2026-02-03 16:25:38 +0530

    a

--- backend/src/app.js
+++ backend/src/app.js
@@ -16,7 +16,7 @@ app.use((err, req, res, next) => {
 
   res.status(err.status || 500).json({
     success: false,
-    message: err.message || "Internal Server Error",
+    message: err.message || "Internal Server Errorrr",
   });
 });
 
